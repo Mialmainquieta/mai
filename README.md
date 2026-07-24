1ef0d7bd10a6a29036f3b6a06421be07b1c2e499
Mialmainquieta <mateobrunel@gmail.com>
2026-07-24 19:17:17 -0300

    Crear estructura inicial del Framework MAI

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# mai
-repositorio de mi alma inquieta deco
+# MAI FRAMEWORK
+Este repositorio contiene el Framework MAI, utilizado para personalizar la tienda Mi Alma Inquieta sobre Empretienda Classic.
--- js/mai.js
+++ js/mai.js
@@ -1,6 +1,18 @@
-console.log("🚀 MAI Framework v1.0");
+// ======================================
+// MAI Framework
+// Archivo principal
+// Versión 1.0
+// ======================================
+
+const MAI = {
+
+    version: "1.0"
+
+};
 
 document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("🚀 MAI Framework v1.0");
 
     console.log("MAI conectado correctamente");
 
