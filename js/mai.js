328447572ee05ecad4def71363c66452773e12c4
Mialmainquieta <mateobrunel@gmail.com>
2026-07-24 17:00:51 -0300

    Add DOMContentLoaded event listener for MAI Framework

--- js/mai.js
+++ js/mai.js
@@ -1,1 +1,9 @@
 console.log("🚀 MAI Framework iniciado");
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("MAI conectado correctamente");
+
+    document.body.style.outline = "4px solid red";
+
+});
