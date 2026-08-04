2f8bba13a20086f66a0ed7497dffcaedf6745433
Mialmainquieta <mateobrunel@gmail.com>
2026-08-04 00:13:33 -0300

    Update index.js

--- js/modules/layout/index.js
+++ js/modules/layout/index.js
@@ -31,54 +31,12 @@ MAI.modules.layout = {
 
     renderFooterHeader() {
 
-        return `
-            <div class="mai-footer-header"></div>
-        `;
+    return `
+        <div class="mai-footer-header">
+            <h2>TEST</h2>
+        </div>
+    `;
 
-    }
-
-};
-// ======================================
-//
-// MAI Framework
-// Módulo Layout
-// Versión 1.0.0
-//
-// ======================================
-
-MAI.modules.layout = {
-
-    init() {
-
-        this.insertarFooterHeader();
-
-    },
-
-    insertarFooterHeader() {
-
-        const footerTop = document.querySelector(".footer__top");
-
-        if (!footerTop) return;
-
-        if (document.querySelector(".mai-footer-header")) return;
-
-        footerTop.insertAdjacentHTML(
-            "afterbegin",
-            this.renderFooterHeader()
-        );
-
-    },
-
-    renderFooterHeader() {
-
-        return `
-            <div class="mai-footer-header">
-
-                <h2>MAI Footer Header funcionando 🚀</h2>
-
-            </div>
-        `;
-
-    }
+}
 
 };
