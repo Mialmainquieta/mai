40b38d4518898a7be1c94cfb5e17d06d74ae8f3e
Mialmainquieta <mateobrunel@gmail.com>
2026-08-04 13:18:05 -0300

    cambios

--- js/modules/layout/index.js
+++ js/modules/layout/index.js
@@ -37,35 +37,27 @@ MAI.modules.layout = {
 
         <div class="mai-footer-grid">
 
-            <!-- ==========================================
-                            MARCA
-            =========================================== -->
-
             <div class="mai-footer-brand">
 
-                <img
-                    src="https://mialmainquieta.github.io/mai/Img/logo-footer.png"
-                    alt="Mi Alma Inquieta"
-                    class="mai-footer-logo">
+                <a href="/" class="mai-footer-logo-link">
+
+                    <img
+                        src="https://mialmainquieta.github.io/mai/Img/logo-footer.png"
+                        alt="Mi Alma Inquieta"
+                        class="mai-footer-logo">
+
+                </a>
 
                 <p class="mai-footer-claim">
-
                     Calidez y calidad cerca tuyo.
-
                 </p>
 
                 <p class="mai-footer-description">
-
                     Diseño artesanal<br>
                     Deco • Textiles • Cestería
-
                 </p>
 
             </div>
-
-            <!-- ==========================================
-                            COMPRÁ
-            =========================================== -->
 
             <div class="mai-footer-column">
 
@@ -74,24 +66,15 @@ MAI.modules.layout = {
                 <ul>
 
                     <li><a href="https://www.mialmainquieta.com.ar/">Tienda</a></li>
-
                     <li><a href="https://www.mialmainquieta.com.ar/cesteria">Cestería</a></li>
-
                     <li><a href="https://www.mialmainquieta.com.ar/textiles">Textiles</a></li>
-
                     <li><a href="https://www.mialmainquieta.com.ar/accesorios">Detalles Deco</a></li>
-
                     <li><a href="https://www.mialmainquieta.com.ar/mai-outfit">MAI Outfit</a></li>
-
                     <li><a href="https://www.mialmainquieta.com.ar/combos-y-ofertas">Combos y ofertas</a></li>
 
                 </ul>
 
             </div>
-
-            <!-- ==========================================
-                            AYUDA
-            =========================================== -->
 
             <div class="mai-footer-column">
 
@@ -99,43 +82,9 @@ MAI.modules.layout = {
 
                 <ul>
 
-                    <li><a href="/como-comprar">Cómo comprar</a></li>
-
+                    <li><a href="#">Cómo comprar</a></li>
                     <li><a href="https://www.mialmainquieta.com.ar/cambios-y-devoluciones">Cambios y devoluciones</a></li>
-
-                    <li><a href="/preguntas-frecuentes">Preguntas frecuentes</a></li>
-
-                </ul>
-
-            </div>
-
-            <!-- ==========================================
-                            SEGUINOS
-            =========================================== -->
-
-            <div class="mai-footer-column">
-
-                <h3>SEGUINOS</h3>
-
-                <ul>
-
-                    <li>
-                        <a href="https://www.instagram.com/mialmainquieta" target="_blank">
-                            Instagram
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="https://api.whatsapp.com/send/?phone=5491153365338&text&type=phone_number&app_absent=0" target="_blank">
-                            WhatsApp
-                        </a>
-                    </li>
-
-                    <li>
-                        <a href="#">
-                            Facebook
-                        </a>
-                    </li>
+                    <li><a href="#">Preguntas frecuentes</a></li>
 
                 </ul>
 
